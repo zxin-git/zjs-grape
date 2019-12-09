54b0189e14eda56b0bb69251576f3e2a6a0f218e
zxin <zxin.info@gmail.com>
2019-12-09 21:04:01 +0800

    update logback & jackson location

--- src/main/java/com/zxin/java/spring/BootApplication.java
+++ src/main/java/com/zxin/java/spring/BootApplication.java
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  *
  */
 @SpringBootApplication
-@ComponentScan("com.zxin.java.spring.mybatis")
+@ComponentScan("com.zxin.java.spring.jackson")
 public class BootApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(BootApplication.class);
--- src/test/java/com/zxin/java/spring/jackson/json/TreeModelTest.java
+++ src/test/java/com/zxin/java/spring/jackson/json/TreeModelTest.java
@@ -3,14 +3,19 @@ package com.zxin.java.spring.jackson.json;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class TreeModelTest extends TreeModel {
 
-	private static final Logger logger = LoggerFactory.getLogger(TreeModelTest.class);
+//	private static final Logger logger = LoggerFactory.getLogger(TreeModelTest.class);
 
 
 	@Test
